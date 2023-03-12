bb0856329d23730f2abb0f03220b23e7cdfcd483
elkwans <carlosriquelmedeveloper@gmail.com>
2023-03-11 19:34:17 -0600

    Intento de creacion de factory

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Carlos Alberto Riquelme Valdivia
 
 ## 2857176
+
+Este proyecto es una practica para una escuela que desea hacer una base de datos donde se registren los cursos de una escuela de robotica.
+
+Se adjuntara el diagrama ER anterior para la entrega de la actividad.
--- database/migrations/2023_03_12_003026_create_cursos_table.php
+++ database/migrations/2023_03_12_003026_create_cursos_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
             $table->string('nombreCurso');
-            $table->integer('idKit');
             $table->timestamps();
         });
     }
